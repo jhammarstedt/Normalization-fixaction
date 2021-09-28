7f65f718d73becb29e87c1443fe5be46fd76333f
Matej Sestak <matej.sestak@hotmail.com>
2021-09-28 10:51:56 +0200

    added minmax, variable scaling, made other more general

--- normal_methods.py
+++ normal_methods.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from sys import platform
 import pandas as pd
 import numpy as np
@@ -12,65 +13,85 @@ try:
     from plotly.subplots import make_subplots
 except ImportError:
     pass
-LINUX = False  # ! just quick fix for now
+
+SEPARATOR = '\\' if platform == 'win32' else '/'
 
 
 def z_score(data):
     data_copy = data.copy()
-    scaler = StandardScaler()
     data_without_cat = data_copy.select_dtypes(exclude='category')
+
+    scaler = StandardScaler()
+    data_copy[data_without_cat.columns] = scaler.fit_transform(data_without_cat)
+
+    return data_copy
+
+
+def tanh_norm(data):
+    # Reduce influence of the values in the tail of the distribution
+    # x = 0.5 * (tanh((0.01(x - mu)) / std) + 1)
+
+    data_copy = data.copy()
+    data_without_cat = data_copy.select_dtypes(exclude='category')
+
+    m = np.mean(data_without_cat, axis=0)
+    std = np.std(data_without_cat, axis=0)
+
+    data_copy[data_without_cat.columns] = 0.5 * (np.tanh(0.01 * ((data_without_cat - m) / std)) + 1)
+    return data_copy
+
+
+def min_max(data):
+    data_copy = data.copy()
+    data_without_cat = data_copy.select_dtypes(exclude='category')
+
+    scaler = MinMaxScaler()
     data_copy[data_without_cat.columns] = scaler.fit_transform(data_without_cat)
     return data_copy
 
 
-def tanh_norm(df):
-    # Reduce influence of the values in the tail of the distribution
-
-    m = np.mean(df.iloc[:, :-1], axis=0)  # array([16.25, 26.25])
-    std = np.std(df.iloc[:, :-1], axis=0)  # array([17.45530005, 22.18529919])
-
-    tanh_df = 0.5 * (np.tanh(0.01 * ((df.iloc[:, :-1] - m) / std)) + 1)
-    return tanh_df
-
-
-def min_max(data):
-    scaler = MinMaxScaler()
-    data.iloc[:, :-1] = scaler.fit_transform(data.iloc[:, :-1])
-    return data
-
-
-def pareto_scaling(df):
-    data_copy = df.copy()
+def pareto_scaling(data):
+    data_copy = data.copy()
     data_without_cat = data_copy.select_dtypes(exclude='category')
+
     mean = np.mean(data_without_cat, axis=0)
     std = np.std(data_without_cat, axis=0)
+
     data_copy[data_without_cat.columns] = (data_without_cat - mean) / np.sqrt(std)
     return data_copy
 
 
-def variable_stability_scaling():
-    pass
+def variable_scaling(data):
+    # extends the z-score normalization by introducing the Co- efficient of Variation (CV) as a scaling factor.
+    # The coefficient of variation is given as the ratio of the mean of data to its standard deviation
+    data_copy = data.copy()
+    data_without_cat = data_copy.select_dtypes(exclude='category')
+
+    mean = np.mean(data_without_cat, axis=0)
+    std = np.std(data_without_cat, axis=0)
+
+    data_copy[data_without_cat.columns] = ((data_without_cat - mean) * mean) / (std ** 2)
+    return data_copy
 
 
 def load_data(dataset: str):
     cwd = os.getcwd()
-    separator = '\\' if platform == 'win32' else '/'
     if dataset == "wine":
         dtypes = {'fixed acidity': 'float64', 'volatile acidity': 'float64', 'citric acid': 'float64',
                   'residual sugar': 'float64', 'chlorides': 'float64', 'free sulfur dioxide': 'float64',
                   'total sulfur dioxide': 'float64', 'density': 'float64', 'pH': 'float64', 'sulphates': 'float64',
                   'alcohol': 'float64', 'quality': 'int64'}
-        if not LINUX:
-            w = pd.read_csv(r"datasets\wine\winequality-white.csv", delimiter=";", dtype=dtypes)
-            r = pd.read_csv(r"datasets\wine\winequality-red.csv", delimiter=";", dtype=dtypes)
-        else:
-            raise NotImplementedError("Fix linux env")
+
+        w = pd.read_csv(rf"datasets{SEPARATOR}wine{SEPARATOR}winequality-white.csv", delimiter=";", dtype=dtypes)
+        r = pd.read_csv(rf"datasets{SEPARATOR}wine{SEPARATOR}winequality-red.csv", delimiter=";", dtype=dtypes)
+
         r['color'] = 'red'
         w['color'] = 'white'
         df = r.append(w)
         df = df.sample(frac=1).reset_index(drop=True)
         print(df.head())
         df.color = df.color.astype('category')
+
     elif dataset == "adult":
         column_names = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'occupation',
                         'relationship',
@@ -81,11 +102,11 @@ def load_data(dataset: str):
                   'race': 'category', 'sex': 'category',
                   'capital_gain': np.int64, 'capital_loss': np.int64, 'hours_per_week': np.int64,
                   'native_country': 'category', 'income': 'category'}
-        df = pd.read_csv(cwd + separator + 'datasets' + separator + 'adult' + separator + 'adult.data', delimiter=', ',
+        df = pd.read_csv(cwd + SEPARATOR + 'datasets' + SEPARATOR + 'adult' + SEPARATOR + 'adult.data', delimiter=', ',
                          names=column_names, dtype=dtypes, index_col=False)
     elif dataset == "comp":
         df = pd.read_csv(
-            cwd + separator + 'datasets' + separator + 'comp' + separator + 'machine.data',
+            cwd + SEPARATOR + 'datasets' + SEPARATOR + 'comp' + SEPARATOR + 'machine.data',
             delimiter=",",
             header=None)
         df = df.iloc[:, 2:]
@@ -95,7 +116,7 @@ def load_data(dataset: str):
     return df
 
 
-class Normalizator():
+class Normalizator:
     def __init__(self, dataset: str):
         """
         Class that reads data and performs normalization methods
@@ -115,6 +136,7 @@ class Normalizator():
         # just to make sure the data is not tampered with before normalization
         # if reset is true, the data is reset to the original state
         # if save is true, the data is saved to a new file in the post_norma_data folder, the relative path
+        logging.info(f'Normalizing with {method}')
         if reset:
             # Load the data again
             self.df = load_data(self.dataset)
@@ -123,17 +145,17 @@ class Normalizator():
         elif method == "tanh":
             self.df_norm = tanh_norm(self.df)
         elif method == "pareto":
-            print("pareto scaling")
             self.df_norm = pareto_scaling(self.df)
+        elif method == 'minmax':
+            self.df_norm = min_max(self.df)
+        elif method == 'variableScaling':
+            self.df_norm = variable_scaling(self.df)
         else:
             raise NotImplementedError("This normalization is not implemented yet")
 
         if save:
             # save the data to a new file
-            if not LINUX:
-                self.df_norm.to_csv(rf"output\post_norma_data\{self.dataset}_{method}.csv", index=False)
-            else:
-                self.df_norm.to_csv(rf"output/post_norm_data/{self.dataset}_{method}.csv")
+            self.df_norm.to_csv(rf"output{SEPARATOR}post_norma_data{SEPARATOR}{self.dataset}_{method}.csv", index=False)
 
     def visuals(self):
         fig, axes = plt.subplots(3, 4, figsize=(15, 15))
@@ -145,7 +167,7 @@ class Normalizator():
 
         plt.show()
 
-    def lblEncoder(self, col):
+    def label_encoder(self, col):
         le = LabelEncoder()
         le.fit(self.df[col])
         self.df[col] = le.transform(self.df[col])
@@ -154,7 +176,7 @@ class Normalizator():
 
 if __name__ == "__main__":
     n = Normalizator("adult")
-    n.normalize(method="zscore", save=True)
+    n.normalize(method="minmax", save=True)
     print(n.df.describe())
     print(n.df_norm.describe())
     # n.visuals()
