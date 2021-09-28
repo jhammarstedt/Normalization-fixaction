0af14c43248aec74c62eb0c173cebfc1cb50ed3e
jhammarstedt <johan.hammarstedt@gmail.com>
2021-09-28 09:20:27 +0200

    added basic models

--- basic_models.py
+++ basic_models.py
@@ -9,37 +9,123 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC,SVR
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from xgboost.sklearn import XGBRegressor
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+class ModelClass():
+    def __init__(self,data:dict) -> None:
+        self.datasets = data
+        
 
 
-def run_model(self,model=None):
-    if not model:
-        raise TypeError("Need to specify model type")
-    elif model =="knn":
-        categorical = self.df_norm.select_dtypes('category')
-        self.df_norm[categorical.columns] = categorical.apply(LabelEncoder().fit_transform)
-        X = self.df_norm.iloc[:,:-1]
-        y = self.df_norm.iloc[:,-1]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        knn = KNeighborsClassifier(n_neighbors=3)
-        knn.fit(X_train, y_train)
-        y_pred = knn.predict(X_test)
-        print(confusion_matrix(y_test, y_pred))
-        print(classification_report(y_test, y_pred))
+    def run_models(self):
+         
+        #model_class =["knn","logreg","svm"]
+        for dataset_name in self.datasets.keys():
+            df = self.datasets[dataset_name]["data"].copy() #copy dataframe 
 
-if __name__ == "__main__":
+            if self.datasets[dataset_name]["pred_type"] =="regression":
+                model_class = ["svr","XGBR"]
+            else: #classification
+                model_class = ["svm","knn","logreg"]
+
+            categorical = df.select_dtypes('category')
+            
+            df[categorical.columns] = categorical.apply(LabelEncoder().fit_transform)
+            target = self.datasets[dataset_name]["target"]
+            X = df.drop([target], axis=1)
+            y = df[target]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+            for model in model_class:
+                self.train_model(X_train,X_test,y_train,y_test,model=model)            
+            
+    def evaluate(self,y_test,y_pred, type="regression"):
+        if type == "regression":
+            mae = mean_absolute_error(y_test, y_pred)
+            mse = mean_squared_error(y_test, y_pred)
+            r2 = r2_score(y_test, y_pred)
+            print("The model performance for testing set")
+            print("--------------------------------------")
+            print('MAE is {}'.format(mae))
+            print('MSE is {}'.format(mse))
+            print('R2 score is {}'.format(r2))
+        else:
+            print("Classification report")
+            print(classification_report(y_test, y_pred))
+            print("Confusion matrix")
+            print(confusion_matrix(y_test, y_pred))
+            print("Accuracy score")
+            print(accuracy_score(y_test, y_pred))
+
+               
+    def train_model(self,X_train,X_test,y_train,y_test,model=None):
+        if not model:
+            raise TypeError("Need to specify model type")
+        elif model =="knn":
+            print("**********KNN************")
+            knn = KNeighborsClassifier(n_neighbors=3)
+            knn.fit(X_train, y_train)
+            y_pred = knn.predict(X_test)
+            self.evaluate(y_test,y_pred,type="classification")
+            print("************************")
+
+        elif model == "svm":
+            # SVM
+            print("**********SVM************") 
+            svm = SVC()
+            svm.fit(X_train, y_train)
+            y_pred = svm.predict(X_test)
+            self.evaluate(y_test,y_pred,type="classification")
+            print("************************")
+        elif model=="logreg":
+            # Logistic Regression
+            print("**********Logistic Regression************")
+            logreg = LogisticRegression()
+            logreg.fit(X_train, y_train)
+            y_pred = logreg.predict(X_test)
+            self.evaluate(y_test,y_pred,type="classification")
+            print("***************************")
+        elif model == "XGBR":
+            # XGBoost Regressor
+            print("**********XGBoost Regressor************")
+            xgb = XGBRegressor()
+            xgb.fit(X_train, y_train)
+            y_pred = xgb.predict(X_test)
+            self.evaluate(y_test,y_pred,type="regression")
+            print("***************************")
+        elif model == "svr":
+            # SVR
+            print("**********SVR************")
+            svr = SVR()
+            svr.fit(X_train, y_train)
+            y_pred = svr.predict(X_test)
+            self.evaluate(y_test,y_pred,type="regression")
+            print("***************************")
+        else:
+            raise TypeError("Model type not supported")
+
+def read_data()->dict:
     path = os.path.join(os.getcwd(),"output\post_norma_data")
     files = glob.glob(os.path.join(path,"*.csv"))
     config = json.load(open("dataset_config.json"))["datasets"]
+    datasets = {}
     for f in files:
         dataset_name = (os.path.basename(f).split('_'))[0]
         dtype = config[dataset_name]["dtype"]
         df = pd.read_csv(f,dtype=dtype)
-    
-    print(df.head())
-    #run_model()
+        datasets[dataset_name] = {"data":df,"target":config[dataset_name]["target"],"pred_type":config[dataset_name]["pred_type"],"pred_type":config[dataset_name]["pred_type"]}
+        if dataset_name=="wine":
+            df["color"] = df["color"].astype('category')
+        
+    return datasets
+
+if __name__ == "__main__":
+    data = read_data()
+    model = ModelClass(data)
+    model.run_models()
--- run_pipeline.py
+++ run_pipeline.py
@@ -3,13 +3,38 @@ import sys
 import argparse
 parser = argparse.ArgumentParser()
 import normal_methods,basic_models
+from normal_methods import Normalizator
 
-parser.add_argument("-a","--all",default = "all_data",help="Run with all datasets")
-args = parser.parse_args()
+
+#parser.add_argument("-a","--all",default = "all_data",help="Run with all datasets")
+#args = parser.parse_args()
 
 if __name__ == "__main__":
-    data_file = sys.argv[1]
-    print(data_file)
-    print(args.all)
+    """Here we run the pipeline
+    we can run with all datasets or with a specific one
+    """
+
+    #NORMALIZATION - MAT
+    #Write the input and output of your normalization method here
+    #! Output results in output/post_norma_data with proper names (e.g wine_zscore.csv)
+    
+    
+
+
+
+    #BASIC MODELS - JOHAN
+    #* Reads normalized data from output/post_norma_data in the specified format and runs classifiers
+    #* Reads the unnormalized data from datasets
+
+
+
+    #Adv MODELS - JOAO
+
+    # EVALUATION - ISAK
+    # Write the input you want to evaluate here and the output you will produce
+    
+    #data_file = sys.argv[1]
+    #print(data_file)
+    # print(args.all)
     #normal_methods.run_normal_methods(data_file)
     #basic_models.run_basic_models(data_file)
